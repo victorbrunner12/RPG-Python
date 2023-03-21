34055580cb3347aa3cd9436cc353b3bfe2b04dab
victorbrunner12 <vitorandreymartinsbrunner@hotmail.com>
2023-03-20 23:54:55 -0300

    Jogo RPG com Lobos e Goblins se atacando.

---
+++ RPG.py
@@ -0,0 +1,70 @@
+class SerVivo:
+    def __init__(self, pontos_vida, pontos_ataque):
+        self.pontos_vida = pontos_vida
+        self.pontos_ataque = pontos_ataque
+    
+class Personagem(SerVivo):
+    def __init__(self, pontos_vida, pontos_ataque, nome):
+        super().__init__(pontos_vida, pontos_ataque)
+        self.nome = nome
+
+    def atacar(self, personagem_atacado):
+        try:
+            if self.verifica_personagem_vivo(self, personagem_atacado):
+                if personagem_atacado.nome.lower() == 'goblin':
+                    personagem_atacado.pontos_vida -= self.pontos_ataque
+                    print(f"{self.nome} atacou {personagem_atacado.nome} com {self.pontos_ataque} pontos de ataque.\n{personagem_atacado.nome} agora tem {personagem_atacado.pontos_vida} de pontos de vida.\n")
+                elif personagem_atacado.nome.lower() == 'lobo mau':
+                    personagem_atacado.pontos_vida -= self.pontos_ataque 
+                    print(f"{self.nome} atacou {personagem_atacado.nome} com {self.pontos_ataque} pontos de ataque.\n{personagem_atacado.nome} agora tem {personagem_atacado.pontos_vida} de pontos de vida.\n")
+        except Exception as ex:
+            print(ex)
+
+    def super_ataque(self, personagem_atacando, personagem_atacado):
+        try:
+            if self.verifica_personagem_vivo(personagem_atacando, personagem_atacado):
+                if personagem_atacado.nome.lower() == 'goblin':
+                    dano = personagem_atacando.pontos_ataque*personagem_atacando.forca / personagem_atacado.inteligencia
+                    personagem_atacado.pontos_vida -= dano
+                    print(f"{self.nome} atacou {personagem_atacado.nome} com super poder.\n{personagem_atacado.nome} agora tem {personagem_atacado.pontos_vida} de pontos de vida.\n")
+                elif personagem_atacado.nome.lower() == 'lobo mau':
+                    dano = personagem_atacando.pontos_ataque*personagem_atacando.inteligencia / personagem_atacado.forca
+                    personagem_atacado.pontos_vida -= dano
+                    print(f"{self.nome} atacou {personagem_atacado.nome} com super poder.\n{personagem_atacado.nome} agora tem {personagem_atacado.pontos_vida} de pontos de vida.\n")
+        except Exception as ex:
+            print(ex)
+
+    def verifica_personagem_vivo(self, personagem_atacando, personagem_atacado):
+        try:
+            if personagem_atacando.pontos_vida <= 0 or personagem_atacado.pontos_vida <= 0:
+                print(f"A Batalha acabou! O {personagem_atacando.nome if personagem_atacando.pontos_vida==0 else personagem_atacado.nome} morreu!")
+                return False
+            return True
+        except Exception as ex:
+            print(ex)
+
+class Monstro(Personagem):
+    def __init__(self, pontos_vida, pontos_ataque, nome, tipo):
+        super().__init__(pontos_vida, pontos_ataque, nome)
+        self.tipo = tipo
+
+class Lobo(Monstro):
+    def __init__(self, pontos_vida, pontos_ataque, nome, tipo, forca):
+        super().__init__(pontos_vida, pontos_ataque, nome, tipo)
+        self.forca = forca
+
+class Goblin(Monstro):
+    def __init__(self, pontos_vida, pontos_ataque, nome, tipo, inteligencia):
+        super().__init__(pontos_vida, pontos_ataque, nome, tipo)
+        self.inteligencia = inteligencia
+
+lobo = Lobo(pontos_vida=100, pontos_ataque=45, nome='Lobo Mau', tipo='Lobo Grande', forca=100)
+goblin = Goblin(pontos_vida=950, pontos_ataque=3, nome='Goblin', tipo='Goblin Ancião', inteligencia=100)
+
+print(f"Vida goblin: {goblin.pontos_vida}\nVida Lobo: {lobo.pontos_vida}\n")
+
+lobo.super_ataque(personagem_atacando=lobo, personagem_atacado=goblin)
+goblin.super_ataque(personagem_atacando=goblin, personagem_atacado=lobo)
+lobo.atacar(personagem_atacado=goblin)
+goblin.atacar(personagem_atacado=lobo)
+
---
+++ readme.txt
@@ -0,0 +1,33 @@
+A classe base "SerVivo" é definida com dois atributos inteiros: pontos_vida e pontos_ataque, que representam os pontos de vida e ataque do ser vivo. O atributo booleano personagem_vivo é definido como verdadeiro.
+
+A classe "Personagem" é uma classe derivada de "SerVivo" com um atributo adicional nome, uma string que representa o nome do personagem. Ela também possui dois métodos, "atacar" e "super_ataque", que executam um ataque básico e um ataque especial, respectivamente, em um personagem ou monstro.
+
+O método "atacar" executa um ataque básico baseado em um dano calculado da seguinte forma:
+	"self.pontos_ataque" é a quantidade de pontos de ataque do personagem atacante.
+	"personagem_atacado.pontos_vida" é a quantidade de pontos de vida do personagem sendo atacado.
+
+	A formula para o dano é simplesmente a quantidade de pontos de ataque do personagem atacante menos
+	a quantidade de pontos de vida do personagem sendo atacado.
+	
+
+O método "super_ataque" executa um ataque calculado, levando em consideração o seu atributo adicional:
+	
+	"personagem_atacando.pontos_ataque": A quantidade de pontos de ataque que o personagem que está atacando possui.
+	"personagem_atacando.inteligencia": A inteligência do personagem que está atacando.
+	"personagem_atacado.forca": A força do personagem que está sendo atacado.
+	
+	A fórmula utiliza essas variáveis para determinar a quantidade de dano que o ataque causará ao personagem que está sendo atacado.
+
+	--personagem_atacando.pontos_ataque * personagem_atacando.inteligencia / personagem_atacado.forca--
+
+	OBS: Somente Lobo possui o atributo "forca" e somente o Goblin possui o atributo "inteligencia". Caso o Lobo esteja atacando o Goblin fica:
+	
+	--personagem_atacando.pontos_ataque * personagem_atacando.forca / personagem_atacado.inteligencia--
+
+A classe "Monstro" é uma classe derivada de "Personagem" com um atributo adicional tipo, que representa o tipo de monstro.
+
+A classe "Lobo" é uma classe derivada de "Monstro" com um atributo adicional forca, que representa a força do lobo.
+
+A classe "Goblin" é uma classe derivada de "Monstro" com um atributo adicional inteligencia, que representa a inteligência do goblin.
+
+As instâncias lobo e goblin são criadas com os atributos apropriados e seus pontos de vida iniciais são impressos na tela.
